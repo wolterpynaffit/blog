03cb6763825f251f71a4608735799d3e19fe4046
wolterpynaffit <63312655+wolterpynaffit@users.noreply.github.com>
2022-11-20 09:59:58 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "jekyll", "~> 4.3.1"
+
+# gem "webrick", "~> 1.7" # Uncomment if you are using ruby ~> 3.0.0
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_includes/**/*.{html,js,md}',
+    './_layouts/**/*.{html,js,md}',
+    './_*/*.{html,js,md}',
+    './*.{html,js,md}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
